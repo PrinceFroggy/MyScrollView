7753a84f3d29d2699939e9bb71bf2712d284a32a
PrinceFroggy <asolesa@yahoo.ca>
2017-03-22 09:12:29 -0400

    Go!

--- MyScrollView/MyScrollView.m
+++ MyScrollView/MyScrollView.m
@@ -16,24 +16,11 @@
 
 @implementation MyScrollView
 
--(instancetype)initWithCoder:(NSCoder *)aDecoder
-{
-    self = [super initWithCoder:aDecoder];
-    
-    _panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-    
-    [self addGestureRecognizer:self.panRecognizer];
-    
-    return self;
-}
-
 - (void)handlePan:(UIPanGestureRecognizer *)recognizer
 {
     if (recognizer.state == UIGestureRecognizerStateChanged)
     {
-        CGPoint translation = [recognizer translationInView:self];
         
-        [self setBounds:CGRectMake(-translation.x, -translation.y, self.frame.size.width, self.frame.size.height)];
     }
 }
 
